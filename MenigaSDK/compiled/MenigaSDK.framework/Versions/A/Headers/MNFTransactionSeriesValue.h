d04fb3f8e3572763a4959d6ab52df8e846af4f90
Haukur Ísfeld <haukur@meniga.net>
2017-05-02 13:56:49 +0000

    compiled framework

---
+++ MenigaSDK/compiled/MenigaSDK.framework/Versions/A/Headers/MNFTransactionSeriesValue.h
@@ -0,0 +1,45 @@
+//
+//  MNFTransactionSeriesValue.h
+//  Meniga-ios-sdk
+//
+//  Created by Haukur Ísfeld on 08/03/2017.
+//  Copyright © 2017 Meniga. All rights reserved.
+//
+
+#import "MNFObject.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+/**
+ The MNFTransactionSeriesValue class represents transaction series values in an object.
+ */
+@interface MNFTransactionSeriesValue : MNFObject
+
+/**
+ The net amount of all the transactions in the period
+ */
+@property (nonatomic,strong,readonly) NSNumber *nettoAmount;
+
+/**
+ The total positive amount of all the transactions in the period.
+ */
+@property (nonatomic,strong,readonly) NSNumber *totalPositive;
+
+/**
+ The total negative amount of all the transactions in the period.
+ */
+@property (nonatomic,strong,readonly) NSNumber *totalNegative;
+
+/**
+ The start date of the period.
+ */
+@property (nonatomic,strong,readonly) NSDate *date;
+
+/**
+ The transaction ids of all the transactions in the period.
+ */
+@property (nonatomic,strong,readonly) NSArray <NSNumber*> *transactionIds;
+
+@end
+
+NS_ASSUME_NONNULL_END
