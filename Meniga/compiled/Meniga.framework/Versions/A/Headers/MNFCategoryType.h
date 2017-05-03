8dea5d03adf8da4c06944f6f7ba6819ee8e335a5
Haukur Ísfeld <haukur@meniga.net>
2017-05-03 13:45:52 +0000

    Renamed framework

---
+++ Meniga/compiled/Meniga.framework/Versions/A/Headers/MNFCategoryType.h
@@ -0,0 +1,25 @@
+//
+//  MNFCategoryType.h
+//  Meniga-ios-sdk
+//
+//  Created by Mathieu Grettir Skulason on 4/11/16.
+//  Copyright © 2016 Meniga. All rights reserved.
+//
+
+#import "MNFObject.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+/**
+ @abstract An MNFCategoryType object has an identifier and name. The identifier property is an NSNumber inherited from the MNFObject and is used as a unique identifier. The name better describes the category type and its function.
+ */
+@interface MNFCategoryType : MNFObject
+
+/**
+ @abstract A parameter with the name of the category type
+ */
+@property (nonatomic, readonly, strong) NSString *name;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Meniga/compiled/Meniga.framework/Versions/A/Headers/MNFScheduledEvent.h
@@ -0,0 +1,103 @@
+//
+//  MNFScheduledEvent.h
+//  Meniga-ios-sdk
+//
+//  Created by Mathieu Grettir Skulason on 6/10/16.
+//  Copyright © 2016 Meniga. All rights reserved.
+//
+
+#import "MNFObject.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@class MNFTransactionDayOverview;
+
+typedef NS_ENUM(NSInteger, MNFScheduleInterval) {
+    Weekly,
+    Monthly
+};
+
+@interface MNFScheduledEvent : MNFObject
+
+/**
+ @abstract The total expense of the period of the scheduled event.
+ */
+@property (nonatomic, strong, readonly) NSNumber *totalExpenses;
+
+/**
+ @abstract The total income of the the period of the scheduled event.
+ */
+@property (nonatomic, strong, readonly) NSNumber *totalIncome;
+
+/**
+ @abstract The total expense per category id over the scheduled event period.
+ */
+@property (nonatomic, strong, readonly) NSDictionary *expensesPerCategory;
+
+/**
+ @abstract The total income per category id over the scheduled event period.
+ */
+@property (nonatomic, strong, readonly) NSDictionary *incomePerCategory;
+
+/**
+ @abstract The number of transactions per merchant id.
+ */
+@property (nonatomic, strong, readonly) NSDictionary *transactionCountPerMerchant;
+
+/**
+ @abstract A string describing whether it's a 'Weekly' or 'Monthly' scheduled event. Other types may be introduced with time.
+ */
+@property (nonatomic, strong, readonly) NSString *scheduledEventType;
+
+/**
+ @abstract The start date of the scheduled event.
+ */
+@property (nonatomic, strong, readonly) NSDate *startDate;
+
+/**
+ @abstract The end date of the scheduled event.
+ */
+@property (nonatomic, strong, readonly) NSDate *endDate;
+
+/**
+ @abstract A list of transactions per day over the scheduled period with additional data about each day that can be presented to the user in various forms. Look at MNFTransactionDayOverview object for more information about its properties.
+ */
+@property (nonatomic, strong, readonly) NSArray <MNFTransactionDayOverview *> *transactionsPerDay;
+
+/**
+ @abstract The topic id of the scheduled event.
+ */
+@property (nonatomic, strong, readonly) NSNumber *topicId;
+
+/**
+ @abstract The date the scheduled event was generated on.
+ */
+@property (nonatomic, strong, readonly) NSDate *date;
+
+/**
+ @abstract The name of the topic this scheduled event relates to. Example would be "info" or "transaction".
+ */
+@property (nonatomic, strong, readonly) NSString *topicName;
+
+/**
+ @abstract The title of the event such as weekly report.
+ */
+@property (nonatomic, strong, readonly) NSString *title;
+
+/**
+ @acstract The body of the event.
+ */
+@property (nonatomic, strong, readonly) NSString *body;
+
+/**
+ @abstract The type name should always be ScheduledFeedItemModel.
+ */
+@property (nonatomic, strong, readonly) NSString *typeName;
+
++ (MNFJob*)fetchWithId:(NSNumber *)identifier completion:(nullable MNFScheduledEventCompletionHandler)completion;
+
+- (MNFJob*)refreshWithCompletion:(nullable MNFErrorOnlyCompletionHandler)completion;
+
+@end
+
+NS_ASSUME_NONNULL_END
