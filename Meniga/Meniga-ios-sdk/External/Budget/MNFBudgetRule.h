cd0448ca2b0fa5292b30ba2b8cbf1c97485d7064
Haukur Ísfeld <haukur@meniga.net>
2018-11-27 13:38:26 +0000

    Header documentation

--- Meniga/Meniga-ios-sdk/External/Budget/MNFBudgetRule.h
+++ Meniga/Meniga-ios-sdk/External/Budget/MNFBudgetRule.h
@@ -54,6 +54,19 @@ NS_ASSUME_NONNULL_BEGIN
 ///******************************
 /// @name Fetching
 ///******************************
+
+/**
+ Fetches a list of budget rules.
+ 
+ @param budgetId The id of the budget the rules are in.
+ @param categoryIds The category ids the rules are associated with.
+ @param startDate The start date for the rules.
+ @param endDate The end date for the rules.
+ @param allowOverlappingRules Whether rules can overlap the start and end date parameters.
+ @param completion A completion block returning a list of budget rules and an error.
+ 
+ @return MNFJob A job containing a list of budget rules and an error.
+ */
 + (MNFJob*)fetchRulesWithBudgetId:(NSNumber*)budgetId
                       categoryIds:(nullable NSString*)categoryIds
                         startDate:(nullable NSDate*)startDate
@@ -64,6 +77,21 @@ NS_ASSUME_NONNULL_BEGIN
 ///******************************
 /// @name Creating
 ///******************************
+
+/**
+ Creates a list of budget rules.
+ 
+ @param budgetId The id of the budget the rules are in.
+ @param categoryIds The category ids the rules are associated with.
+ @param startDate The start date for the rules.
+ @param endDate The end date for the rules.
+ @param generationType The generation type for the rules.
+ @param monthInterval The monthly interval of a recurring pattern for the rules.
+ @param repeatUntil The end of the recurrence of the rules.
+ @param completion A completion block returning a list of budget rules and an error.
+ 
+ @return MNFJob A job containing a list of budget rules and an error.
+ */
 + (MNFJob*)budgetRuleWithBudgetId:(NSNumber*)budgetId
                       categoryIds:(nullable NSString*)categoryIds
                         startDate:(nullable NSDate*)startDate
@@ -73,6 +101,17 @@ NS_ASSUME_NONNULL_BEGIN
                       repeatUntil:(nullable NSDate*)repeatUntil
                        completion:(nullable MNFMultipleBudgetRulesCompletionHandler)completion;
 
+///******************************
+/// @name Deleting
+///******************************
+
+/**
+ Deletes a budget rule.
+ 
+ @param completion A completion block returning an error.
+ 
+ @return MNFJob A job containing an error.
+ */
 - (MNFJob*)deleteWithCompletion:(nullable MNFErrorOnlyCompletionHandler)completion;
 @end
 
