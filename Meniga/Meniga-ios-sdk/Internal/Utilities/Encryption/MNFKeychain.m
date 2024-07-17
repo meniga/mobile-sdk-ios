8ac41a36d4d44935ee39d9e83c35384419f0e33e
Dentro2000 <kowalski.szymon@outlook.com>
2024-07-17 16:04:38 +0200

    MOBILE-4583: replace deprecated methods in MNFKeychain

--- Meniga/Meniga-ios-sdk/Internal/Utilities/Encryption/MNFKeychain.m
+++ Meniga/Meniga-ios-sdk/Internal/Utilities/Encryption/MNFKeychain.m
@@ -11,18 +11,20 @@
 
 @implementation MNFKeychain
 
+
+#pragma mark Encryption key storage
 + (void)saveData:(NSData *)data forKey:(NSString *)key {
+    
     NSDictionary *dataDictionary =
-        [NSDictionary dictionaryWithObjectsAndKeys:[NSKeyedArchiver archivedDataWithRootObject:data],
-                                                   (__bridge id)kSecValueData,
-                                                   nil];
+        [NSDictionary dictionaryWithObjectsAndKeys:[self archiveData:data], (__bridge id)kSecValueData, nil];
+
     NSMutableDictionary *keyChainItem = [[self p_fetchEncryptionKeychainItemForKey:key] mutableCopy];
     OSStatus error = SecItemUpdate((__bridge CFDictionaryRef)keyChainItem, (__bridge CFDictionaryRef)dataDictionary);
 
     if (error != noErr) {
         MNFLogDebug(@"Encryption key not found in keychain, creating new one");
         MNFLogVerbose(@"Encryption key not found in keychain, creating new one");
-        [keyChainItem setObject:[NSKeyedArchiver archivedDataWithRootObject:data] forKey:(__bridge id)kSecValueData];
+        [keyChainItem setObject:[self archiveData:data] forKey:(__bridge id)kSecValueData];
         error = SecItemAdd((__bridge CFDictionaryRef)keyChainItem, NULL);
         if (error != noErr) {
             MNFLogError(@"Saving encryption key to keychain failed. Error: %@", error);
@@ -30,6 +32,18 @@
         }
     }
 }
+
++ (nullable NSData *)archiveData:(NSData *) data {
+    NSError *archiverError = nil;
+    NSData *archivedData = [NSKeyedArchiver archivedDataWithRootObject:data requiringSecureCoding:YES error:&archiverError];
+    
+    if (archiverError) {
+        MNFLogError(@"Archiving data error. Error: %@", archiverError.localizedDescription);
+    }
+    
+    return archivedData;
+}
+
 + (id)loadDataForKey:(NSString *)key {
     NSMutableDictionary *keychainItem = [[self p_fetchEncryptionKeychainItemForKey:key] mutableCopy];
     [keychainItem setObject:(id)kCFBooleanTrue forKey:(__bridge id)kSecReturnData];
@@ -37,10 +51,11 @@
     CFDataRef keyData = NULL;
     id returnValue = nil;
 
+    
     OSStatus error = SecItemCopyMatching((__bridge CFDictionaryRef)keychainItem, (CFTypeRef *)&keyData);
     if (error == noErr) {
         @try {
-            returnValue = [NSKeyedUnarchiver unarchiveObjectWithData:(__bridge id)keyData];
+            returnValue = [self unarchiveData:keyData];
         } @catch (NSException *exception) {
             MNFLogDebug(@"Unarchiving encryption key failed. Exception: %@", exception);
         } @finally {
@@ -50,6 +65,20 @@
 
     return returnValue;
 }
+
++ (nullable id)unarchiveData:(CFDataRef) keyData {
+    NSError *unarchiverError = nil;
+    id returnValue = nil;
+    returnValue = [NSKeyedUnarchiver unarchivedObjectOfClass:[NSData class] fromData:(__bridge id)keyData error:&unarchiverError];
+    if (unarchiverError) {
+        MNFLogError(@"Unarchiving encryption key failed. Error: %@", unarchiverError.localizedDescription);
+        return returnValue;
+    }
+    
+    return returnValue;
+}
+
+
 + (void)deleteDataForKey:(NSString *)key {
     NSDictionary *keychainItem = [self p_fetchEncryptionKeychainItemForKey:key];
     OSStatus error = SecItemDelete((__bridge CFDictionaryRef)keychainItem);
@@ -59,18 +88,22 @@
     }
 }
 
+#pragma mark Password storage
+
 + (void)savePassword:(NSString *)password {
     NSDictionary *dataDictionary =
-        [NSDictionary dictionaryWithObjectsAndKeys:[NSKeyedArchiver archivedDataWithRootObject:password],
+    [NSDictionary dictionaryWithObjectsAndKeys:[self archivePassword:password],
                                                    (__bridge id)kSecValueData,
                                                    nil];
+    
+    
     NSMutableDictionary *keyChainItem = [[self p_fetchPasswordKeychainItem] mutableCopy];
     OSStatus error = SecItemUpdate((__bridge CFDictionaryRef)keyChainItem, (__bridge CFDictionaryRef)dataDictionary);
 
     if (error != noErr) {
         MNFLogError(@"Password key not found in keychain, creating new one");
         MNFLogVerbose(@"Password key not found in keychain, creating new one");
-        [keyChainItem setObject:[NSKeyedArchiver archivedDataWithRootObject:password]
+        [keyChainItem setObject:[self archivePassword:password]
                          forKey:(__bridge id)kSecValueData];
         error = SecItemAdd((__bridge CFDictionaryRef)keyChainItem, NULL);
         if (error != noErr) {
@@ -79,6 +112,18 @@
         }
     }
 }
+
++ (nullable NSData *)archivePassword:(NSString *) string {
+    NSError *archiverError = nil;
+    NSData *archivedData = [NSKeyedArchiver archivedDataWithRootObject:string requiringSecureCoding:YES error:&archiverError];
+    
+    if (archiverError) {
+        MNFLogError(@"Archiving password error. Error: %@", archiverError.localizedDescription);
+    }
+    
+    return archivedData;
+}
+
 + (NSString *)loadPassword {
     NSMutableDictionary *keychainItem = [[self p_fetchPasswordKeychainItem] mutableCopy];
     [keychainItem setObject:(id)kCFBooleanTrue forKey:(__bridge id)kSecReturnData];
@@ -89,7 +134,7 @@
     OSStatus error = SecItemCopyMatching((__bridge CFDictionaryRef)keychainItem, (CFTypeRef *)&keyData);
     if (error == noErr) {
         @try {
-            returnValue = [NSKeyedUnarchiver unarchiveObjectWithData:(__bridge id)keyData];
+            returnValue = [self unarchivePassword:keyData];
         } @catch (NSException *exception) {
             MNFLogDebug(@"Unarchiving password key failed. Exception: %@", exception);
         } @finally {
@@ -99,6 +144,19 @@
 
     return returnValue;
 }
+
++ (nullable id)unarchivePassword:(CFDataRef) keyData {
+    NSError *unarchiverError = nil;
+    id returnValue = nil;
+    returnValue = [NSKeyedUnarchiver unarchivedObjectOfClass:[NSString class] fromData:(__bridge id)keyData error:&unarchiverError];
+    if (unarchiverError) {
+        MNFLogError(@"Unarchiving password key failed. Error: %@", unarchiverError.localizedDescription);
+        return returnValue;
+    }
+    
+    return returnValue;
+}
+
 + (void)deletePassword {
     NSDictionary *keychainItem = [self p_fetchPasswordKeychainItem];
     OSStatus error = SecItemDelete((__bridge CFDictionaryRef)keychainItem);
@@ -107,9 +165,11 @@
     }
 }
 
+#pragma mark Token storage
+
 + (void)saveToken:(NSDictionary *)token {
     NSDictionary *dataDictionary =
-        [NSDictionary dictionaryWithObjectsAndKeys:[NSKeyedArchiver archivedDataWithRootObject:token],
+        [NSDictionary dictionaryWithObjectsAndKeys:[self archiveDictionaryData:token],
                                                    (__bridge id)kSecValueData,
                                                    nil];
     NSMutableDictionary *keyChainItem = [[self p_fetchEncryptionKeychainItemForKey:@"RefreshToken"] mutableCopy];
@@ -117,13 +177,25 @@
 
     if (error != noErr) {
         MNFLogDebug(@"Token key not found in keychain, creating new one");
-        [keyChainItem setObject:[NSKeyedArchiver archivedDataWithRootObject:token] forKey:(__bridge id)kSecValueData];
+        [keyChainItem setObject:[self archiveDictionaryData:token] forKey:(__bridge id)kSecValueData];
         error = SecItemAdd((__bridge CFDictionaryRef)keyChainItem, NULL);
         if (error != noErr) {
             MNFLogDebug(@"Saving token key to keychain failed. Error: %@", error);
         }
     }
 }
+
++ (nullable NSData *)archiveDictionaryData:(NSDictionary *) dictionary {
+    NSError *archiverError = nil;
+    NSData *archivedData = [NSKeyedArchiver archivedDataWithRootObject:dictionary requiringSecureCoding:YES error:&archiverError];
+    
+    if (archiverError) {
+        MNFLogError(@"Archiving data error. Error: %@", archiverError.localizedDescription);
+    }
+    
+    return archivedData;
+}
+
 + (NSDictionary *)loadToken {
     NSMutableDictionary *keychainItem = [[self p_fetchEncryptionKeychainItemForKey:@"RefreshToken"] mutableCopy];
     [keychainItem setObject:(id)kCFBooleanTrue forKey:(__bridge id)kSecReturnData];
@@ -134,7 +206,7 @@
     OSStatus error = SecItemCopyMatching((__bridge CFDictionaryRef)keychainItem, (CFTypeRef *)&keyData);
     if (error == noErr) {
         @try {
-            returnValue = [NSKeyedUnarchiver unarchiveObjectWithData:(__bridge id)keyData];
+            returnValue = [self unarchiveToken:keyData];
         } @catch (NSException *exception) {
             MNFLogDebug(@"Unarchiving token key failed. Exception: %@", exception);
         } @finally {
@@ -144,6 +216,19 @@
 
     return returnValue;
 }
+
++ (nullable id)unarchiveToken:(CFDataRef) keyData {
+    NSError *unarchiverError = nil;
+    id returnValue = nil;
+    returnValue = [NSKeyedUnarchiver unarchivedObjectOfClass:[NSDictionary class] fromData:(__bridge id)keyData error:&unarchiverError];
+    if (unarchiverError) {
+        MNFLogError(@"Unarchiving token key failed. Error: %@", unarchiverError.localizedDescription);
+        return returnValue;
+    }
+    
+    return returnValue;
+}
+
 + (void)deleteToken {
     NSDictionary *keychainItem = [self p_fetchEncryptionKeychainItemForKey:@"RefreshToken"];
     OSStatus error = SecItemDelete((__bridge CFDictionaryRef)keychainItem);
